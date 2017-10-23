1429ff6c3c190ffa9df84f27158bc2d03d7fa498
Adam Driscoll <adam.r.driscoll@gmail.com>
2017-10-22 21:09:45 -0500

    Adding module

---
+++ Selenium.psm1
@@ -0,0 +1,167 @@
+[System.Reflection.Assembly]::LoadFrom("$PSScriptRoot\WebDriver.dll")
+[System.Reflection.Assembly]::LoadFrom("$PSScriptRoot\WebDriver.Support.dll")
+
+<#
+.SYNOPSIS
+Starts a Selenium Chrome driver
+
+.DESCRIPTION
+Starts a Selenium Chrome driver
+
+.EXAMPLE
+Start-SeChrome
+#>
+function Start-SeChrome {
+    New-Object -TypeName "OpenQA.Selenium.Chrome.ChromeDriver"
+}
+
+<#
+.SYNOPSIS
+Starts a Selenium Firefox driver
+
+.DESCRIPTION
+Starts a Selenium Firefox driver
+
+.EXAMPLE
+Start-SeFirefox
+#>
+function Start-SeFirefox {
+    New-Object -TypeName "OpenQA.Selenium.Firefox.FirefoxDriver"
+}
+
+<#
+.SYNOPSIS
+Stops a Selenium driver.
+
+.DESCRIPTION
+Stops a Selenium driver.
+
+.PARAMETER Driver
+The driver to stop.
+#>
+function Stop-SeDriver {
+    param($Driver) 
+
+    $Driver.Dispose()
+}
+
+<#
+.SYNOPSIS
+Navigates to a url.
+
+.DESCRIPTION
+Navigates to a url.
+
+.PARAMETER Driver
+The driver to navigate with. See Start-SeChrome and Start-SeFirefox.
+
+.PARAMETER Url
+The URL to navigate to. 
+
+.EXAMPLE
+Enter-SeUrl -Url https://www.google.com -Driver (Start-SeChrome)
+#>
+
+function Enter-SeUrl {
+    param($Driver, $Url)
+
+    $Driver.Navigate().GoToUrl($Url)
+}
+
+<#
+.SYNOPSIS
+Find an element in the currently loaded page.
+
+.DESCRIPTION
+Find an element in the currently loaded page.
+
+.PARAMETER Driver
+The driver to navigate with. See Start-SeChrome and Start-SeFirefox.
+
+.PARAMETER Name
+The name of the element to find. 
+
+.PARAMETER Id
+The Id of the element to find. 
+
+.PARAMETER ClassName
+The ClassName of the element to find. 
+
+.PARAMETER LinkText
+The LinkText of the element to find. 
+
+.EXAMPLE
+$Element = Find-SeElement -Driver $Driver -Id "MyTextbox"
+#>
+function Find-SeElement {
+    param(
+        [Parameter()]
+        $Driver,
+        [Parameter(ParameterSetName = "ByName")]
+        $Name,
+        [Parameter(ParameterSetName = "ById")]
+        $Id,
+        [Parameter(ParameterSetName = "ByClassName")]
+        $ClassName,
+        [Parameter(ParameterSetName = "ByLinkText")]
+        $LinkText)
+
+    Process {
+        if ($PSCmdlet.ParameterSetName -eq "ByName") {
+            $Driver.FindElement([OpenQA.Selenium.By]::Name($Name))
+        }
+
+        if ($PSCmdlet.ParameterSetName -eq "ById") {
+            $Driver.FindElement([OpenQA.Selenium.By]::Id($Id))
+        }
+
+        if ($PSCmdlet.ParameterSetName -eq "ByLinkText") {
+            $Driver.FindElement([OpenQA.Selenium.By]::LinkText($LinkText))
+        }
+
+        if ($PSCmdlet.ParameterSetName -eq "ByClassName") {
+            $Driver.FindElement([OpenQA.Selenium.By]::ClassName($ClassName))
+        }
+    }
+}
+
+<#
+.SYNOPSIS
+Clicks an element
+
+.DESCRIPTION
+Clicks an element
+
+.PARAMETER Element
+The element to click.
+
+.EXAMPLE
+Invoke-SeClick -Element $Element
+#>
+function Invoke-SeClick {
+    param([OpenQA.Selenium.IWebElement]$Element)
+
+    $Element.Click()
+}
+
+<#
+.SYNOPSIS
+Sends keys to an element
+
+.DESCRIPTION
+Sends keys to an element
+
+.PARAMETER Element
+The element to send keys to. 
+
+.PARAMETER Keys
+The keys to send.
+
+.EXAMPLE
+Send-SeKeys -Element $Element -Keys "Hey, there!"
+#>
+function Send-SeKeys {
+    param([OpenQA.Selenium.IWebElement]$Element, [string]$Keys)
+
+    $Element.SendKeys($Keys)
+}
