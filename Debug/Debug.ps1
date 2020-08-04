9d14d39edb3badbfcae891b24733179b2bf6a59d
Francis Mercier <Francis@nzt.ca>
2020-08-03 21:21:07 -0400

    quick documentation on how to debug  / compileé

--- Debug/Debug.ps1
+++ Debug/Debug.ps1
@@ -3,14 +3,19 @@ import-module InvokeBuild
 
 # Just a line to replace $PsScriptRoot since the former does not work when debugging with F8
 $ScriptFullPath, $ScriptRoot, $ScriptName, $ScriptNameShort = @('', '', '', ''); $ScriptFullPath = if (-not [String]::IsNullOrEmpty($PSCommandPath)) { $PSCommandPath } elseif ($psEditor -ne $null) { $psEditor.GetEditorContext().CurrentFile.Path } elseif ($psise -ne $null) { $psise.CurrentFile.FullPath }; $ScriptRoot = Split-Path -Path $ScriptFullPath -Parent; $ScriptName = Split-Path -Path $ScriptFullPath -Leaf; $ScriptNameShort = ([System.IO.Path]::GetFileNameWithoutExtension($ScriptFullPath))
-
+$ProjectPath = Split-Path $ScriptRoot 
 
 # Load Debug version
-import-module "$ScriptRoot\Selenium.psd1" -Force
+import-module "$ProjectPath\Selenium.psd1" -Force
 
 #Load Compiled version
-#import-module "$ScriptRoot\output\selenium\Selenium.psd1" -Force
+import-module "$ProjectPath\output\selenium\Selenium.psd1" -Force
 
 
 # Build module
-#invoke-build -File "$ScriptRoot\Selenium.build.ps1"
+invoke-build -File "$ProjectPath\Selenium.build.ps1"
+
+
+
+
+
