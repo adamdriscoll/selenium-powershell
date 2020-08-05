2d3648f80d5c114f3b725ad6c78d1a16ac7dc6fa
Francis Mercier <Francis@nzt.ca>
2020-08-05 06:54:16 -0400

    Update Combined.tests.ps1

--- Examples/Combined.tests.ps1
+++ Examples/Combined.tests.ps1
@@ -63,7 +63,8 @@ $BrowserOptText = Build-StringFromHash $BrowserOptHash
 Describe "Testing the tailspin toys demo site at $env:SITE_URL" {
     BeforeAll {
         #Relying on environment variable to pick the browser. Capture ID for use in logs by requesting verbose and redirecting it.
- 
+        Write-Verbose $env:SITE_URL -Verbose
+   
         $BrowserID = SeOpen -URL $env:SITE_URL -Options  $Global:BrowserOptHash 4>&1
         $BrowserID = ($BrowserID.Message -replace '^Opened ', '') + ' on ' + [System.Environment]::OSVersion.Platform
     }
