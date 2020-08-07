89d55fb8db55f700fd4fac419db1b6b6f8cff3a9
Francis Mercier <Francis@nzt.ca>
2020-08-07 15:52:06 -0400

    #68

--- Public/Stop-SeDriver.ps1
+++ Public/Stop-SeDriver.ps1
@@ -7,12 +7,21 @@ function Stop-SeDriver {
         [OpenQA.Selenium.IWebDriver]
         $Target = $Global:SeDriver
     )
-
+    $TextInfo = (Get-Culture).TextInfo
     if (($null -ne $Target) -and ($Target -is [OpenQA.Selenium.IWebDriver])) {
-        Write-Verbose -Message "Closing $($Target.Capabilities.browsername)..."
+        $BrowserName = $TextInfo.ToTitleCase($Target.Capabilities.browsername)
+
+        if ($null -eq $Target.SessionId) {
+            Write-Warning "$BrowserName Driver already closed"
+            return $null
+        }
+
+        Write-Verbose -Message "Closing $BrowserName..."
         $Target.Close()
-        $Target.Dispose()
+        $Target.Dispose()    
+        
+        
         if ($Target -eq $Global:SeDriver) { Remove-Variable -Name SeDriver -Scope global }
     }
-    else { throw "A valid <IWebDriver> Target must be provided." }
+    else { Write-Warning 'A valid <IWebDriver> must be provided.' }
 }
