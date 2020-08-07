6f958daeb821abb701557f839ea726aafc63e1d3
Joel Sallow (/u/ta11ow) <32407840+vexx32@users.noreply.github.com>
2020-08-07 12:59:15 -0400

    Add Get/Push/Pop-SeUrl commands (#104)
    * :sparkles: Add Get/Push/Pop-SeUrl commands
    
    * :recycle: Rename Open-SeUrl to Set-SeUrl
    
    Match the added commands' names better and improve discoverability.
    Open-SeUrl added back as an alias to avoid breaking existing scripts.
    
    Also added CBH to the existing function.

---
+++ Public/Pop-SeUrl.ps1
@@ -0,0 +1,35 @@
+function Pop-SeUrl {
+    <#
+    .SYNOPSIS
+    Navigate back to the most recently pushed URL in the location stack.
+
+    .DESCRIPTION
+    Retrieves the most recently pushed URL from the location stack and navigates
+    to that URL with the specified or default driver.
+
+    .EXAMPLE
+    Pop-SeUrl
+
+    Retrieves the most recently pushed URL and navigates back to that URL.
+
+    .NOTES
+    A separate internal location stack is maintained for each driver instance
+    by the module. This stack is completely separate from the driver's internal
+    Back/Forward history logic.
+
+    To utilise a driver's Back/Forward functionality, instead use Set-SeUrl.
+    #>
+    [CmdletBinding()]
+    [Alias('Pop-SeLocation')]
+    param(
+        [Parameter(ValueFromPipeline = $true)]
+        [Alias("Driver")]
+        [ValidateIsWebDriverAttribute()]
+        $Target = $Global:SeDriver
+    )
+    process {
+        if ($Script:SeLocationMap[$Target].Count -gt 0) {
+            Set-SeUrl -Url $Script:SeLocationMap[$Target].Pop() -Target $Target
+        }
+    }
+}
