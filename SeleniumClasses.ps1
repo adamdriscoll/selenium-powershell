7da57de3008e708c85dd348f3751d6e73c6af9f8
Francis Mercier <Francis@nzt.ca>
2020-08-12 07:53:53 -0400

    v4

--- SeleniumClasses.ps1
+++ SeleniumClasses.ps1
@@ -24,25 +24,7 @@ if ('ValidateIsWebDriverAttribute' -as [type]) {
     class ValidateIsWebDriver : ValidateIsWebDriverAttribute {}
 }
 
-if ('ByTransformAttribute' -as [type]) {
-    #Allow BY to shorten cssSelector, ClassName, LinkText, and TagName
-    class ByTransformAttribute : System.Management.Automation.ArgumentTransformationAttribute {
-        [object] Transform([System.Management.Automation.EngineIntrinsics]$EngineIntrinsics, [object] $InputData) {
-            if ($inputData -match 'CssSelector|Name|Id|ClassName|LinkText|PartialLinkText|TagName|XPath') {
-                return $InputData
-            }
-            switch -regex ($InputData) {
-                "^css" { return 'CssSelector'; break }
-                "^class" { return 'ClassName'  ; break }
-                "^link" { return 'LinkText'   ; break }
-                "^tag" { return 'TagName'    ; break }
-            }
-            return $InputData
-        }
-    }
 
-    class ByTransform : ByTransformAttribute {}
-}
 
 if ('OperatorTransformAttribute' -as [type]) {
     #Allow operator to use containing, matching, matches, equals etc.
@@ -137,5 +119,16 @@ enum SeWindowState {
     Minimized
     Maximized
     Fullscreen
+}
+
+enum SeBySelector {
+    ClassName
+    CssSelector
+    Id
+    LinkText
+    PartialLinkText
+    Name
+    TagName
+    XPath
 }
 
