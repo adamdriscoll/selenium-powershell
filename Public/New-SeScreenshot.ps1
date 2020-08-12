71f0021f7433a8a539cdf268b9c6cc7f6f90a3ab
Francis Mercier <Francis@nzt.ca>
2020-08-12 04:35:35 -0400

    Duplicate function cleanup
    + upgrade existing ones if needed.

--- Public/New-SeScreenshot.ps1
+++ Public/New-SeScreenshot.ps1
@@ -1,29 +1,24 @@
 function New-SeScreenshot {
-    [Alias('SeScreenshot')]
     [cmdletbinding(DefaultParameterSetName = 'Path')]
     param(
         [Parameter(ParameterSetName = 'Path' , Position = 0, Mandatory = $true)]
-        [Parameter(ParameterSetName = 'PassThru', Position = 0)]
         $Path,
 
         [Parameter(ParameterSetName = 'Path', Position = 1)]
-        [Parameter(ParameterSetName = 'PassThru', Position = 1)]
         [OpenQA.Selenium.ScreenshotImageFormat]$ImageFormat = [OpenQA.Selenium.ScreenshotImageFormat]::Png,
 
         [Parameter(ValueFromPipeline = $true)]
-        [Alias("Driver")]
         [ValidateIsWebDriverAttribute()]
-        $Target = $Script:SeDriversCurrent ,
+        $Driver = $Script:SeDriversCurrent ,
 
         [Parameter(ParameterSetName = 'Base64', Mandatory = $true)]
         [Switch]$AsBase64EncodedString,
-
-        [Parameter(ParameterSetName = 'PassThru', Mandatory = $true)]
-        [Alias('PT')]
         [Switch]$PassThru
     )
-    $Screenshot = [OpenQA.Selenium.Support.Extensions.WebDriverExtensions]::TakeScreenshot($Target)
-    if ($AsBase64EncodedString) { $Screenshot.AsBase64EncodedString }
+    $Screenshot = [OpenQA.Selenium.Support.Extensions.WebDriverExtensions]::TakeScreenshot($Driver)
+    if ($AsBase64EncodedString) {
+        return $Screenshot.AsBase64EncodedString 
+    }
     elseif ($Path) {
         $Path = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($Path)
         $Screenshot.SaveAsFile($Path, $ImageFormat) 
--- Public/Send-SeClick.ps1
+++ Public/Send-SeClick.ps1
@@ -1,16 +1,25 @@
 function Send-SeClick {
-    [alias('SeClick')]
     param(
         [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 0)]
         [OpenQA.Selenium.IWebElement]$Element,
         [Switch]$JavaScript,
         $SleepSeconds = 0 ,
-        $Driver,
+        $Driver = $script:SeDriversCurrent,
         [switch]$PassThru
     )
     Process {
-        if ($JavaScript) { $Element.WrappedDriver.ExecuteScript("arguments[0].click()", $Element) }
-        else { $Element.Click() }
+        if ($JavaScriptClick) {
+            try {
+                $Driver.ExecuteScript("arguments[0].click()", $Element)
+            }
+            catch {
+                $PSCmdlet.ThrowTerminatingError($_)
+            }
+        }
+        else {
+            $Element.Click()
+        }
+
         if ($SleepSeconds) { Start-Sleep -Seconds $SleepSeconds }
         if ($PassThru) { $Element }
     }
