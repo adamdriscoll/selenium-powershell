4cebe918ee3665c7594ec2c5d508c7b141e90cbb
Francis Mercier <Francis@nzt.ca>
2020-08-13 17:41:43 -0400

    Update CI.ps1

--- CI/CI.ps1
+++ CI/CI.ps1
@@ -12,7 +12,7 @@ if ($Platform -like 'win*6') {
 
 #Make sure we have the modules we need
 
-Import-Module .\Selenium.psd1 -Force -ErrorAction Stop
+Import-Module .\Output\Selenium\Selenium.psd1 -Force -ErrorAction Stop
 $checkImportExcel = Get-Module -ListAvailable ImportExcel
 if (-not ($checkImportExcel)) {
     Write-Verbose -Verbose 'Installing ImportExcel'
