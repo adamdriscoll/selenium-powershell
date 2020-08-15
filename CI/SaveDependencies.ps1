1aec6a07888aa41c33298f817ddcb20f86bcf9ac
Francis Mercier <Francis@nzt.ca>
2020-08-15 08:38:16 -0400

    ci

--- CI/SaveDependencies.ps1
+++
@@ -1,4 +0,0 @@
-﻿New-Item -ItemType Directory -Path './Modules'
-$Params = @{Path = './Modules'; Repository = 'PSGallery' }
-Save-Module -Name Pester -RequiredVersion 4.10.1 @Params
-Save-Module -Name ImportExcel -RequiredVersion 7.1.1 @Paramsb
