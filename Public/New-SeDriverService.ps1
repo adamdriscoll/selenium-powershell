9d56c4c370ab17e21d984fc85c118c8fdfcf4d7b
Francis Mercier <Francis@nzt.ca>
2020-08-22 18:18:25 -0400

    v4

--- Public/New-SeDriverService.ps1
+++ Public/New-SeDriverService.ps1
@@ -33,6 +33,7 @@ function New-SeDriverService {
         Firefox { 
             if ($ServicePath) { $service = [OpenQA.Selenium.Firefox.FirefoxDriverService]::CreateDefaultService($ServicePath) }
             else { $service = [OpenQA.Selenium.Firefox.FirefoxDriverService]::CreateDefaultService() }
+            $service.Host = '::1'
         }
         InternetExplorer { 
             if ($WebDriverPath) { $Service = [OpenQA.Selenium.IE.InternetExplorerDriverService]::CreateDefaultService($WebDriverPath) }
@@ -44,7 +45,7 @@ function New-SeDriverService {
         }
     }
 
-    $service.Host = '::1'
+
     if ($Quiet) { $Service.HideCommandPromptWindow = $true }
 
     return $service
