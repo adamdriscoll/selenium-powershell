c037fc53d30e0d742a86f23de1dc8611241a2a7c
Francis Mercier <Francis@nzt.ca>
2020-08-31 02:41:22 -0400

    Update Combined.tests.ps1

--- Examples/Combined.tests.ps1
+++ Examples/Combined.tests.ps1
@@ -2,16 +2,10 @@
 Describe "Testing the tailspin toys demo site at $env:SITE_URL" {
     BeforeAll {
         . "$(Join-Path -Path $PSScriptRoot -ChildPath '_TestDependencies.ps1' )"
-        Import-Module "$(Join-Path -Path $PSScriptRoot -ChildPath '_TestDependencies.ps1' )"
-        
+      
         $Global:TestCaseSettings = Get-TestCasesSettings 
         $Global:BrowserOptHash = $Global:TestCaseSettings."$Global:DefaultBrowser".DefaultOptions
         $Global:BrowserOptText = Build-StringFromHash $Global:BrowserOptHash
-
-        Write-Verbose '-----' -Verbose
-       
-        Write-Verbose '-----' -Verbose
-
         #Relying on environment variable to pick the browser. Capture ID for use in logs by requesting verbose and redirecting it.
         try { $BrowserID = Start-SeDriver -Browser $Global:DefaultBrowser -StartURL $env:SITE_URL  @Global:BrowserOptHash -Verbose  4>&1 -Quiet -ErrorAction Stop } catch {}
         $BrowserID = ($BrowserID.Message -replace '^Opened ', '') + ' on ' + [System.Environment]::OSVersion.Platform
@@ -31,84 +25,88 @@ Describe "Testing the tailspin toys demo site at $env:SITE_URL" {
 }
 #endregion
 
-# #URLs we will visit in the remaining tests
-# $Global:PSGalleryPage = 'https://www.powershellgallery.com/'
-# $AlertTestPage = 'https://www.w3schools.com/js/tryit.asp?filename=tryjs_alert'
-# $SelectTestPage = 'https://www.w3schools.com/html/tryit.asp?filename=tryhtml_elem_select'
-# $Global:BrowserOptHash = $TestCaseSettings[$Global:DefaultBrowser].DefaultOptions
-# $Global:BrowserOptText = Build-StringFromHash $Global:BrowserOptHash
-# #As before rely on environment variable to pick browser. Capture ID by requesting & redirecting verbose
-# Describe "PsGallery Test" {
-#     BeforeAll {
-      
-#         $BrowserID = Start-SeDriver -Browser $Global:DefaultBrowser -StartURL $Global:PSGalleryPage @Global:BrowserOptHash -Verbose  4>&1 -Quiet -ErrorAction Stop
-#         $BrowserID = ($BrowserID.Message -replace '^Opened ', '') + ' on ' + [System.Environment]::OSVersion.Platform
-#     }
-#     Context "in $BrowserID with settings ($Global:BrowserOptText)" {
-#         It 'opened the browser, saving the webdriver in a global variable          ' {
-#             Get-SeDriver -Current                                          | Should -Not -BeNullOrEmpty
-#             Get-SeDriver -Current                                          | Should -BeOfType [OpenQA.Selenium.Remote.RemoteWebDriver]
-#         }
+#URLs we will visit in the remaining tests
+
+#$AlertTestPage = 'https://www.w3schools.com/js/tryit.asp?filename=tryjs_alert'
+#$SelectTestPage = 'https://www.w3schools.com/html/tryit.asp?filename=tryhtml_elem_select'
+
+#As before rely on environment variable to pick browser. Capture ID by requesting & redirecting verbose
+Describe "PsGallery Test" {
+    BeforeAll {
+         . "$(Join-Path -Path $PSScriptRoot -ChildPath '_TestDependencies.ps1' )"
+        $Global:PSGalleryPage = 'https://www.powershellgallery.com/'
+        $Global:TestCaseSettings = Get-TestCasesSettings 
+        $Global:BrowserOptHash = $TestCaseSettings[$Global:DefaultBrowser].DefaultOptions
+        $Global:BrowserOptText = Build-StringFromHash $Global:BrowserOptHash
+        try { $BrowserID = Start-SeDriver -Browser $Global:DefaultBrowser -StartURL $Global:PSGalleryPage @Global:BrowserOptHash -Verbose  4>&1 -Quiet -ErrorAction Stop }catch {}
+        $BrowserID = ($BrowserID.Message -replace '^Opened ', '') + ' on ' + [System.Environment]::OSVersion.Platform
+    }
+    Context "in $BrowserID with settings ($Global:BrowserOptText)" {
+        It 'opened the browser, saving the webdriver in a global variable          ' {
+            Get-SeDriver -Current                                          | Should -Not -BeNullOrEmpty
+            Get-SeDriver -Current                                          | Should -BeOfType [OpenQA.Selenium.Remote.RemoteWebDriver]
+        }
         
-#         It 'reached the right starting page                                        ' {
-#             #Should have can check alerts, page title, URL or an element on the page
-#             SeShouldHave -URL eq $PSGalleryPage
-#         }
-#         It 'found the "Sign in" link on the home page by partial text              ' {
-#             SeShouldHave -By PartialLinkText 'Sign in' -With href match logon
-#         }
-#         It 'found the search box on the home page by ID                            ' {
-#             SeShouldHave -By Id search
-#         }
-#         It 'found the search box on the home page by Name                          ' {
-#             SeShouldHave -By Name 'q'
-#         }
-#         It 'found the search box on the home page by css selector                  ' {
-#             #can write -By <<mechanism>> [-selection] <<selection text>>
-#             #       or [-selection] <<selection text>> -By <<mechanism>>
-#             SeShouldHave 'input[name=q]' -By CssSelector
-#         }
-#         It 'found the search box on the home page by class name                    ' {
-#             SeShouldHave -By ClassName "search-box"
-#         }
-#         It 'found the search box on the home page by Tagname and typed in it       ' {
-#             #get element, pipe as input element for Typing, pass the element through
-#             #so pester catches 'null or empty' if it was not found
-#             Get-SeElement -By TagName -Value 'input' |
-#                 Send-SeKeys -ClearFirst -Keys "selenium{{Enter}}" -PassThru -SleepSeconds 2    | Should -Not -BeNullorEmpty
-#         }
-#         It 'searched successfully                                                  ' {
-#             $linkpath = '//*[@id="skippedToContent"]/section/div[1]/div[2]/div[2]/section[1]/div/table/tbody/tr/td[1]/div/div[2]/header/div[1]/h1/a'
-#             SeShouldHave -URL                 match 'packages\?q=selenium' -Timeout 15
-#             #Two tests on the same element, second passes it through to click
-#             SeShouldHave $linkpath -With href match selenium
-#             SeShouldHave $linkpath -With Text like *selenium* -PassThru | Send-SeClick -SleepSeconds 5
-#         }
-#         It 'opened the search result page and found the expected content           ' {
-#             #Just to show we can test for the presence of multiple links. Each one is re-tested ...
-#             SeShouldHave '//*[@id="version-history"]/table/tbody[1]/tr[1]/ td[1]/a/b' ,
-#             '//*[@id="skippedToContent"]/section/div/aside/ul[2]/li[1]/a' -Timeout 15
+        It 'reached the right starting page                                        ' {
+            #Should have can check alerts, page title, URL or an element on the page
+            SeShouldHave -URL eq $PSGalleryPage
+        }
+        It 'found the "Sign in" link on the home page by partial text              ' {
+            SeShouldHave -By PartialLinkText 'Sign in' -With href match logon
+        }
+        It 'found the search box on the home page by ID                            ' {
+            SeShouldHave -By Id search
+        }
+        It 'found the search box on the home page by Name                          ' {
+            SeShouldHave -By Name 'q'
+        }
+        It 'found the search box on the home page by css selector                  ' {
+            #can write -By <<mechanism>> [-selection] <<selection text>>
+            #       or [-selection] <<selection text>> -By <<mechanism>>
+            SeShouldHave 'input[name=q]' -By CssSelector
+        }
+        It 'found the search box on the home page by class name                    ' {
+            SeShouldHave -By ClassName "search-box"
+        }
+        It 'found the search box on the home page by Tagname and typed in it       ' {
+            #get element, pipe as input element for Typing, pass the element through
+            #so pester catches 'null or empty' if it was not found
+            Get-SeElement -By TagName -Value 'input' |
+                Send-SeKeys -ClearFirst -Keys "selenium{{Enter}}" -PassThru -SleepSeconds 2    | Should -Not -BeNullorEmpty
+        }
+        It 'searched successfully                                                  ' {
+            $linkpath = '//*[@id="skippedToContent"]/section/div[1]/div[2]/div[2]/section[1]/div/table/tbody/tr/td[1]/div/div[2]/header/div[1]/h1/a'
+            SeShouldHave -URL                 match 'packages\?q=selenium' -Timeout 15
+            #Two tests on the same element, second passes it through to click
+            SeShouldHave $linkpath -With href match selenium
+            SeShouldHave $linkpath -With Text like *selenium* -PassThru | Send-SeClick -SleepSeconds 5
+        }
+        It 'opened the search result page and found the expected content           ' {
+            #Just to show we can test for the presence of multiple links. Each one is re-tested ...
+            SeShouldHave '//*[@id="version-history"]/table/tbody[1]/tr[1]/ td[1]/a/b' ,
+            '//*[@id="skippedToContent"]/section/div/aside/ul[2]/li[1]/a' -Timeout 15
 
-#             SeShouldHave '//*[@id="version-history"]/table/tbody[1]/tr[1]/ td[1]/a/b' -With text match "current"
+            SeShouldHave '//*[@id="version-history"]/table/tbody[1]/tr[1]/ td[1]/a/b' -With text match "current"
 
-#             #Can test with "Get-SeElement | where-object <<complex test>>" rather than "with <<feild>> <<operator>> <<value>>"
-#             SeElement    '//*[@id="skippedToContent"]/section/div/aside/ul[2]/li[1]/a'  |
-#                 Where-Object { ($_.text -like "*Project Site*") -and ($_.GetAttribute('href') -match "selenium") } |
-#                     Send-SeClick -PassThru  | Should -Not -Benullorempty
-#         }
-#         It 'went to Github from the project link on the search result              ' {
-#             SeShouldHave -URL  match 'github' -Timeout 30
-#         }
-#         It 'navigated back to the start page and closed the browser                ' {
-#             Set-SeUrl -Back
-#             Set-SeUrl -Back
-#             Set-SeUrl -Back
-#             SeShouldHave -URL eq $PSGalleryPage -Timeout 30
-#             Stop-SeDriver
-#             Get-SeDriver -Current                                              | Should      -BeNullOrEmpty
-#         }
-#     }
-# }
+            #Can test with "Get-SeElement | where-object <<complex test>>" rather than "with <<feild>> <<operator>> <<value>>"
+            SeElement    '//*[@id="skippedToContent"]/section/div/aside/ul[2]/li[1]/a'  |
+                Where-Object { ($_.text -like "*Project Site*") -and ($_.GetAttribute('href') -match "selenium") } |
+                    Send-SeClick -PassThru  | Should -Not -Benullorempty
+        }
+        It 'went to Github from the project link on the search result              ' {
+            SeShouldHave -URL  match 'github' -Timeout 30
+        }
+        It 'navigated back to the start page and closed the browser                ' {
+            Set-SeUrl -Back
+            Set-SeUrl -Back
+            Set-SeUrl -Back
+            SeShouldHave -URL eq $PSGalleryPage -Timeout 30
+            Stop-SeDriver
+            Get-SeDriver -Current                                              | Should      -BeNullOrEmpty
+        }
+    }
+    AfterAll { Stop-SeDriver }
+}
 # $Global:BrowserOptHash = $TestCaseSettings[$Global:DefaultBrowser].PrivateOptions
 # $Global:BrowserOptText = Build-StringFromHash $Global:BrowserOptHash
 # Describe "Alerts and Selection boxes tests" {
