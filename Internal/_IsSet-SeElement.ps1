da9ce7f5bf0203c4e8b0ea12491b449b2842fdd8
Francis Mercier <Francis@nzt.ca>
2020-09-18 03:55:02 -0400

    Now produce a more approppriate error whenever multiple elements are implicitely selected #13232
    I think the final solution won't be to give out an error but rather to support processing multiple elements passed down implicitely to the pipeline.

--- Internal/_IsSet-SeElement.ps1
+++ Internal/_IsSet-SeElement.ps1
@@ -1,9 +1,9 @@
 ﻿function _IsSet-SeElement {
     [cmdletbinding()]
     param ($Driver, [ref]$Element)
-    if ($null -eq $Driver) {$Driver = Get-SeDriver -Current}
+    if ($null -eq $Driver) { $Driver = Get-SeDriver -Current }
     if ($null -ne $Element.Value) { return $true }
-    if ($null -ne $Driver.SeSelectedElements) {
+    if ($null -ne $Driver.SeSelectedElements -and $Driver.SeSelectedElements.count -eq 1) {
         $Element.Value = $Driver.SeSelectedElements
         return $true
     }
