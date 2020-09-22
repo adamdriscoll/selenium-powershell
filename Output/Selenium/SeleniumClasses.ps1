e44ceb21d6c34e2bf46ff4d3e903fc26b6c712ad
Francis Mercier <Francis@nzt.ca>
2020-09-21 21:44:32 -0400

    compile

--- Output/Selenium/SeleniumClasses.ps1
+++ Output/Selenium/SeleniumClasses.ps1
@@ -2,7 +2,7 @@ using namespace system.Collections
 using namespace System.Collections.Generic
 using namespace System.Management.Automation
 using namespace System.Management.Automation.Language
-
+Add-Type -Assembly System.Drawing
 if ('ValidateURIAttribute' -as [type]) {
     class ValidateURIAttribute :  System.Management.Automation.ValidateArgumentsAttribute {
         [void] Validate([object] $arguments , [System.Management.Automation.EngineIntrinsics]$EngineIntrinsics) {
@@ -293,59 +293,59 @@ Function Get-SeElementsConditionsValidation {
 
 
 #region SizePoint Transformation
-    class SizeTransformAttribute : System.Management.Automation.ArgumentTransformationAttribute {
-        # Implement the Transform() method
-        [object] Transform([System.Management.Automation.EngineIntrinsics]$engineIntrinsics, [object] $inputData) {
-            <#
+class SizeTransformAttribute : System.Management.Automation.ArgumentTransformationAttribute {
+    # Implement the Transform() method
+    [object] Transform([System.Management.Automation.EngineIntrinsics]$engineIntrinsics, [object] $inputData) {
+        <#
             The parameter value(s) are passed in here as $inputData. We aren't accepting array input
             for our function, but it's good to make these fairly versatile where possible, so that
             you can reuse them easily!
         #>
-            $outputData = switch ($inputData) {
-                { $_ -is [SizeTransformAttribute] } { $_ }
-                { $_ -is [int] } { [System.Drawing.Size]::new($_, $_) }
-                { $_ -is [string] -and (($_ -split '[,x]').count -eq 2) } { 
-                    $sp = $_ -split '[,x]'
-                    [System.Drawing.Size]::new($sp[0],$sp[1]) 
-                    }
-                default {
-                    # If we hit something we can't convert, throw an exception
-                    throw [System.Management.Automation.ArgumentTransformationMetadataException]::new(
-                        "Could not convert input '$_' to a valid Size object."
-                    )
-                }
+        $outputData = switch ($inputData) {
+            { $_ -is [SizeTransformAttribute] } { $_ }
+            { $_ -is [int] } { [System.Drawing.Size]::new($_, $_) }
+            { $_ -is [string] -and (($_ -split '[,x]').count -eq 2) } { 
+                $sp = $_ -split '[,x]'
+                [System.Drawing.Size]::new($sp[0], $sp[1]) 
             }
-
-            return $OutputData
+            default {
+                # If we hit something we can't convert, throw an exception
+                throw [System.Management.Automation.ArgumentTransformationMetadataException]::new(
+                    "Could not convert input '$_' to a valid Size object."
+                )
+            }
         }
 
+        return $OutputData
     }
 
-    class PointTransformAttribute : System.Management.Automation.ArgumentTransformationAttribute {
-        # Implement the Transform() method
-        [object] Transform([System.Management.Automation.EngineIntrinsics]$engineIntrinsics, [object] $inputData) {
-            <#
+}
+
+class PointTransformAttribute : System.Management.Automation.ArgumentTransformationAttribute {
+    # Implement the Transform() method
+    [object] Transform([System.Management.Automation.EngineIntrinsics]$engineIntrinsics, [object] $inputData) {
+        <#
             The parameter value(s) are passed in here as $inputData. We aren't accepting array input
             for our function, but it's good to make these fairly versatile where possible, so that
             you can reuse them easily!
         #>
-            $outputData = switch ($inputData) {
-                { $_ -is [PointTransformAttribute] } { $_ }
-                { $_ -is [int] } { [System.Drawing.Point]::new($_, $_) }
-                { $_ -is [string] -and (($_ -split '[,x]').count -eq 2) } { 
-                    $sp = $_ -split '[,x]'
-                    [System.Drawing.Point]::new($sp[0],$sp[1]) 
-                    }
-                default {
-                    # If we hit something we can't convert, throw an exception
-                    throw [System.Management.Automation.ArgumentTransformationMetadataException]::new(
-                        "Could not convert input '$_' to a valid Point object."
-                    )
-                }
+        $outputData = switch ($inputData) {
+            { $_ -is [PointTransformAttribute] } { $_ }
+            { $_ -is [int] } { [System.Drawing.Point]::new($_, $_) }
+            { $_ -is [string] -and (($_ -split '[,x]').count -eq 2) } { 
+                $sp = $_ -split '[,x]'
+                [System.Drawing.Point]::new($sp[0], $sp[1]) 
             }
-
-            return $OutputData
+            default {
+                # If we hit something we can't convert, throw an exception
+                throw [System.Management.Automation.ArgumentTransformationMetadataException]::new(
+                    "Could not convert input '$_' to a valid Point object."
+                )
+            }
         }
 
-    }    
+        return $OutputData
+    }
+
+}    
 #endregion
