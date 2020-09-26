a7947c0c3a21616420f249678338aadc5c8a2aad
Francis Mercier <Francis@nzt.ca>
2020-09-26 12:43:41 -0400

    Get-SeElement  - ByChained
    #116

--- Public/Get-SeElement.ps1
+++ Public/Get-SeElement.ps1
@@ -4,9 +4,9 @@ function Get-SeElement {
         #Specifies whether the selction text is to select by name, ID, Xpath etc
         [ArgumentCompleter( { [Enum]::GetNames([SeBySelector]) })]
         [ValidateScript( { $_ -in [Enum]::GetNames([SeBySelector]) })]
-        [SeBySelector]$By = [SeBySelector]::XPath,
+        [SeBySelector[]]$By = [SeBySelector]::XPath,
         [Parameter(Position = 1, Mandatory = $true)]
-        [string]$Value,
+        [string[]]$Value,
         #Specifies a time out
         [Parameter(Position = 2, ParameterSetName = 'Default')]
         [Int]$Timeout = 0,
@@ -24,31 +24,51 @@ function Get-SeElement {
     Begin {
         Init-SeDriver -Driver ([ref]$Driver) -ErrorAction Stop
         $ShowAll = $PSBoundParameters.ContainsKey('All') -and $PSBoundParameters.Item('All') -eq $true
-        
+        if ($By.Count -ne $Value.Count) {
+            Write-Error "The number of `$By $($By.Count) element must match the number of `$value $($value.Count) provided"
+            return 
+        }
         Filter DisplayedFilter([Switch]$All) {
             if ($All) { $_ } else { if ($_.Displayed) { $_ } } 
         }
         $Output = $null
-    }
+    
+        $ResetImplicitTimeout = $null
+        $ByCondition = $null
+        if ($by.Count -gt 1) {
+            $Args = for ($i = 0; $i -lt $by.Count; $i++) {
+                $cby = $by[$i]
+                [OpenQA.Selenium.By]::$cby($value[$i])
+            }
+
+            $ByCondition = [OpenQA.Selenium.Support.PageObjects.ByChained]::new($Args)
+        }
+        else {
+            $ByCondition = [OpenQA.Selenium.By]::$By($Value)
+        }
+        
+        if ($By.Count -gt 1 -or $PSBoundParameters.ContainsKey('Timeout')) {
+            $ResetImplicitTimeout = $Driver.Manage().Timeouts().ImplicitWait
+            $Driver.Manage().Timeouts().ImplicitWait = 0
+        }
+    } 
     process {
       
-        
         
         switch ($PSCmdlet.ParameterSetName) {
             'Default' { 
                 if ($Timeout) {
-                    $TargetElement = [OpenQA.Selenium.By]::$By($Value)
                     $WebDriverWait = [OpenQA.Selenium.Support.UI.WebDriverWait]::new($Driver, (New-TimeSpan -Seconds $Timeout))
-                    $Condition = [OpenQA.Selenium.Support.UI.ExpectedConditions]::PresenceOfAllElementsLocatedBy($TargetElement)
+                    $Condition = [OpenQA.Selenium.Support.UI.ExpectedConditions]::PresenceOfAllElementsLocatedBy($ByCondition)
                     $Output = $WebDriverWait.Until($Condition) | DisplayedFilter -All:$ShowAll
                 }
                 else {
-                    $Output = $Driver.FindElements([OpenQA.Selenium.By]::$By($Value)) | DisplayedFilter -All:$ShowAll
+                    $Output = $Driver.FindElements($ByCondition) | DisplayedFilter -All:$ShowAll
                 }
             }
             'ByElement' {
                 Write-Verbose "Searching an Element - Timeout ignored" 
-                $Output = $Element.FindElements([OpenQA.Selenium.By]::$By($Value)) | DisplayedFilter -All:$ShowAll
+                $Output = $Element.FindElements($ByCondition) | DisplayedFilter -All:$ShowAll
             }
         }
         
@@ -82,18 +102,17 @@ function Get-SeElement {
                     }
                     Add-Member -InputObject $Item -Name 'Attributes' -Value $AttArray -MemberType NoteProperty
                 }
-
-                  
-                    
-
-
-                
             }
               
         }
 
-
         return $Output
+    }
+    End {
+        if ($null -ne $ResetImplicitTimeout) {
+            $Driver.Manage().Timeouts().ImplicitWait = $ResetImplicitTimeout
+        }
+        
     }
 }
 
