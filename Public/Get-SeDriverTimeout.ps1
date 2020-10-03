5d2846d322ca5b2c4c225e957ee0105b4f32c982
Francis Mercier <Francis@nzt.ca>
2020-10-02 21:50:41 -0400

    #150
    Corresponding Get + positionally bound.

--- Public/Get-SeDriverTimeout.ps1
+++ Public/Get-SeDriverTimeout.ps1
@@ -1,18 +1,16 @@
-function Set-SeDriverTimeout {
+function Get-SeDriverTimeout {
     param(
         [Parameter(ValueFromPipeline = $true)]
         [OpenQA.Selenium.IWebDriver]$Driver,
         [Parameter(Position = 0)]
         [ValidateSet('ImplicitWait', 'PageLoad', 'AsynchronousJavaScript')]
-        $TimeoutType = 'ImplicitWait',
-        [Parameter(Position = 1)]
-        [Double]$Timeout = 0
+        $TimeoutType = 'ImplicitWait'
     )
     begin {
         Init-SeDriver -Driver ([ref]$Driver) -ErrorAction Stop
     }
     Process {
-        $Driver.Manage().Timeouts().$TimeoutType = [timespan]::FromMilliseconds($Timeout * 1000)
+        return $Driver.Manage().Timeouts().$TimeoutType 
     }
     End {}
 }
