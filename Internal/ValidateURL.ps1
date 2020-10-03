10c58c5217bf018c2ec8951d73f29bba2d1a6c67
Francis Mercier <Francis@nzt.ca>
2020-10-02 21:39:34 -0400

    Internal function not used.

--- Internal/ValidateURL.ps1
+++
@@ -1,9 +0,0 @@
-function ValidateURL {
-    param(
-        [Parameter(Mandatory = $true)]
-        $URL
-    )
-    $Out = $null
-    #TODO Support for URL without https (www.google.ca) / add it ?
-    [uri]::TryCreate($URL, [System.UriKind]::Absolute, [ref]$Out)
-}
