9b54dbdfc30c176198f58b02db6665de719d48b1
Francis Mercier <Francis@nzt.ca>
2020-10-02 21:41:06 -0400

    Delete ValidateURL.ps1

--- Internal/ValidateURL.ps1
+++
@@ -1,8 +0,0 @@
-function ValidateURL {
-    param(
-        [Parameter(Mandatory = $true)]
-        $URL
-    )
-    $Out = $null
-    [uri]::TryCreate($URL, [System.UriKind]::Absolute, [ref]$Out)
-}
