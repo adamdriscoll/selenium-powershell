5a6174bc06b23e22c74580fcbc3daca729674067
Francis Mercier <Francis@nzt.ca>
2020-10-04 03:35:48 -0400

    #168

--- Public/New-SeScreenshot.ps1
+++ Public/New-SeScreenshot.ps1
@@ -1,18 +1,46 @@
 function New-SeScreenshot {
+    
     [cmdletbinding(DefaultParameterSetName = 'Path')]
     param(
-        [Parameter(ParameterSetName = 'Base64', Mandatory = $true)]
+        [Parameter(DontShow, ValueFromPipeline = $true, ParameterSetName = 'Pipeline')]
+        [ValidateScript( {
+                $Types = @([OpenQA.Selenium.IWebDriver], [OpenQA.Selenium.IWebElement])
+                $Found = $false
+                Foreach ($t in $Types) { if ($_ -is $t) { $Found = $true; break } }
+                if ($found) { return $true } else { Throw "Input must be of one of the following types $($Types -join ',')" }
+            })]
+        $InputObject,
         [Switch]$AsBase64EncodedString,
-        [Parameter(ValueFromPipeline = $true)]
-        [ValidateIsWebDriverAttribute()]
+        [Parameter(ParameterSetName = 'Element')]
+        [ValidateNotNull()]
+        [OpenQA.Selenium.IWebElement]$Element,
+        [Parameter(ParameterSetName = 'Driver')]
+        [ValidateIsWebDriverAttribute()][ValidateNotNull()]
         $Driver
     )
-    Init-SeDriver -Driver ([ref]$Driver) -ErrorAction Stop
-    $Screenshot = [OpenQA.Selenium.Support.Extensions.WebDriverExtensions]::TakeScreenshot($Driver)
-
-    if ($AsBase64EncodedString) {
-        return $Screenshot.AsBase64EncodedString 
+    Begin {
+        Init-SeDriver -Driver ([ref]$Driver) -ErrorAction Stop
     }
+    Process {
+        switch ($PSCmdlet.ParameterSetName) {
+            'Pipeline' {
+                switch ($InputObject) {
+                    { $_ -is [OpenQA.Selenium.IWebElement] } { $Screenshot = $InputObject.GetScreenshot() }
+                    { $_ -is [OpenQA.Selenium.IWebDriver] } { $Screenshot = [OpenQA.Selenium.Support.Extensions.WebDriverExtensions]::TakeScreenshot($InputObject) }
+                }                
+            }
+            'Element' { $Screenshot = $Element.GetScreenshot() }
+            'Driver' { $Screenshot = [OpenQA.Selenium.Support.Extensions.WebDriverExtensions]::TakeScreenshot($Driver) }
+        }
 
-    return $Screenshot
+        if ($AsBase64EncodedString) { 
+            return $Screenshot.AsBase64EncodedString 
+        }
+        else {
+            return $Screenshot
+        }
+    }
+    End {}
+    
+    
 }
