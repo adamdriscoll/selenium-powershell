33ff05f99ece4863cda61f59c7e7b8c9318ca564
Francis Mercier <Francis@nzt.ca>
2020-10-03 23:36:15 -0400

    #161

--- Public/Get-SeElementAttribute.ps1
+++ Public/Get-SeElementAttribute.ps1
@@ -3,9 +3,38 @@ function Get-SeElementAttribute {
         [Parameter(ValueFromPipeline = $true, Mandatory = $true)]
         [OpenQA.Selenium.IWebElement]$Element,
         [Parameter(Mandatory = $true)]
-        [string]$Attribute
+        [string[]]$Attribute
     )
     process {
-        $Element.GetAttribute($Attribute)
+        $AllAttributes = $Attribute.Count -eq 1 -and $Attribute[0] -eq '*'
+        $ManyAttributes = $Attribute.Count -gt 1
+
+        if ($AllAttributes) {
+            $AllAttributes = $Element.WrappedDriver.ExecuteScript('var items = {}; for (index = 0; index < arguments[0].attributes.length; ++index) { items[arguments[0].attributes[index].name] = arguments[0].attributes[index].value }; return items;', $Element)
+            $Output = @{}
+            
+            Foreach ($Att in $AllAttributes.Keys) {
+                $value = $Element.GetAttribute($Att)
+                if ($value -ne "") {
+                    $Output.$Att = $value
+                }
+            }
+            [PSCustomObject]$Output 
+        }
+        elseif ($ManyAttributes) {
+            $Output = @{}
+            Foreach ($Att in $Attribute) {
+                $value = $Element.GetAttribute($Att)
+                if ($value -ne "") {
+                    $Output.$Att = $value
+                }
+            }
+            [PSCustomObject]$Output
+        }
+        else {
+            $Element.GetAttribute($Attribute)
+        }
+
+        
     }
 }
