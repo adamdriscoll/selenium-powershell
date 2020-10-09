bb6a834c5b284e4d3fa94f47a8fa07b7a9ed6b8d
Francis Mercier <Francis@nzt.ca>
2020-10-09 04:29:02 -0400

    Get-SeSelectValue - formatting + other

--- Public/Get-SeSelectValue.ps1
+++ Public/Get-SeSelectValue.ps1
@@ -2,17 +2,47 @@ function Get-SeSelectValue {
     [cmdletbinding(DefaultParameterSetName = 'default')]
     param (
         [Parameter( ValueFromPipeline = $true, Mandatory = $true, Position = 1)]
-        [OpenQA.Selenium.IWebElement]$Element
+        [OpenQA.Selenium.IWebElement]$Element,
+        [Switch]$All
     )
     try {
         $IsMultiSelectResult = [SeleniumSelection.Option]::IsMultiSelect($Element)
         
-        if ($IsMultiSelectResult) {
-            return [SeleniumSelection.Option]::GetAllSelectedOptions($Element).text
+        
+        $SelectStatement = if ($IsMultiSelectResult) { 'GetAllSelectedOptions' } else { 'GetSelectedOption' }
+        $Selected = [SeleniumSelection.Option]::$SelectStatement($Element) 
+        $Items = @(foreach ($item in $Selected) {
+                [PSCustomObject]@{
+                    Text  = $Item.text
+                    Value = Get-SeElementAttribute -Element $Item -Name value
+                }
+            })
+
+        if (-not $All) {
+            return $Items
         }
         else {
-            return [SeleniumSelection.Option]::GetSelectedOption($Element).text
+               
+            $Index = 0
+            $Options = Get-SeElement -Element $Element -By Tagname -Value option -Attributes value
+            $Values = foreach ($Opt in $Options) {
+                [PSCustomObject]@{
+                    Index    = $Index
+                    Text     = $Opt.text
+                    Value    = $opt.Attributes.value
+                    Selected = $Null -ne $Items.Value -and $Items.Value.Contains($opt.Attributes.value)
+                }
+                $Index += 1
+            }
+            return  [PSCustomObject]@{
+                PSTypeName    = 'selenium-powershell/SeSelectValueInfo'
+                IsMultiSelect = [SeleniumSelection.Option]::IsMultiSelect($Element)
+                Items         = $Values
+            }
         }
+
+
+
     }
     catch {
         throw "An error occured checking the selection box, the message was:`r`n $($_.exception.message)"
