c4ef5dc315a4f02d6550a3c5cfe5f8805871725b
Francis Mercier <Francis@nzt.ca>
2020-10-10 23:02:53 -0400

    #166 Removed Invoke-SeKeyAction and modified Invoke-SeKeys instead

--- Public/Invoke-SeKeys.ps1
+++ Public/Invoke-SeKeys.ps1
@@ -1,3 +1,11 @@
+$Script:ModifierKeys = @(
+    'Control',
+    'LeftControl'
+    'Alt',
+    'LeftAlt'
+    'Shift',
+    'LeftShift'
+)
 function Invoke-SeKeys {
     [CmdletBinding(DefaultParameterSetName = 'Default')]
     param(
@@ -17,25 +25,62 @@ function Invoke-SeKeys {
     begin {
         Init-SeDriver -Driver ([ref]$Driver) -ErrorAction Stop
         
-        foreach ($Key in $Script:SeKeys.Name) {
-            $Keys = $Keys -replace "{{$Key}}", [OpenQA.Selenium.Keys]::$Key
+        $Regexstr = '(?<expression>{{(?<value>.*?)}})'
+        $MyMatches = [Regex]::Matches($Keys, $Regexstr)
+        #Treat modifier keys as key down . 
+        $Sequence = [System.Collections.Generic.List[String]]::new()
+        $UseSequence = $Keys.StartsWith('{{')
+
+        Foreach ($m in $MyMatches) {
+            $key = $m.Groups.Item('value').value   
+            $Found = $Script:SeKeys.Name.Contains($value)
+            if ($null -ne $Found) {
+                if ($UseSequence -and $Key -in $Script:ModifierKeys) {
+                    $Sequence.Add([OpenQA.Selenium.Keys]::$key)
+                    $Inputstr = $Inputstr -replace "{{$key}}", ''
+                }
+                else {
+                    $Inputstr = $Inputstr -replace "{{$key}}", [OpenQA.Selenium.Keys]::$key
+                }
+                
+            }
         }
+        $UseSequence = $UseSequence -and $Sequence.Count -gt 0
+        
     }
     process {
+        $Action = [OpenQA.Selenium.Interactions.Actions]::new($Driver)
 
-        if ($PSBoundParameters.ContainsKey('Element')) {
-            if ($ClearFirst) { $Element.Clear() }
-            $Element.SendKeys($Keys)
-            if ($Submit) { $Element.Submit() }
+        switch ($PSBoundParameters.ContainsKey('Element')) {
+            $true {
+                if ($ClearFirst) { $Element.Clear() }
+
+                if ($UseSequence) {
+                    Foreach ($k in $Sequence) { $Action.KeyDown($Element, $k) }
+                    $Action.SendKeys($Element, $Keys)
+                    Foreach ($k in $Sequence) { $Action.KeyUp($Element, $k) }
+                    $Action.Build().Perform()
+                }
+                else {
+                    $Action.SendKeys($Element, $Keys).Perform()
+                }
+
+                if ($Submit) { $Element.Submit() }
+            }
+            $false {
+                if ($UseSequence) {
+                    Foreach ($k in $Sequence) { $Action.KeyDown($k) }
+                    $Action.SendKeys($Keys)
+                    Foreach ($k in $Sequence) { $Action.KeyUp($k) }
+                    $Action.Build().Perform()
+                }
+                else {
+                    $Action.SendKeys($Keys).Perform()
+                }
+            }
         }
-        else {
-            $Action = [OpenQA.Selenium.Interactions.Actions]::new($Driver)
-            $Action.SendKeys($Keys).Perform()
-        }
-       
+      
         if ($Sleep) { Start-Sleep -Milliseconds ($Sleep * 1000) }
         if ($PassThru) { $Element }
     }
 }
-# #KeyDown
-# #KeyUp
