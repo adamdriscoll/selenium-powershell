afc59403dfcbe85b5dd6d6c69d6c2587548080c7
Francis Mercier <Francis@nzt.ca>
2020-10-12 05:50:07 -0400

    fix

--- Internal/Get-DriverProcessId.ps1
+++ Internal/Get-DriverProcessId.ps1
@@ -7,8 +7,8 @@ function Get-DriverProcessId {
     $IsWindowsPowershell = $PSVersionTable.PSVersion.Major -lt 6
 
     if ($IsWindowsPowershell) {
-        $Processes = Get-CimInstance -Class Win32_Process 
-        $BrowserProcess = $Processes.Where( { $_.ParentProcessId -eq $ServiceProcessId -and $_.Name -ne 'conhost.exe' }, 'first').ProcessId
+        $Processes = Get-CimInstance -Class Win32_Process -Filter "ParentProcessId=$ServiceProcessId"
+        $BrowserProcess = $Processes.Where( { $_.Name -ne 'conhost.exe' }, 'first').ProcessId
     }
     else {
         $BrowserProcess = (Get-Process).Where( { { $_.Parent.id -eq $ServiceProcessId -and $_.Name -ne 'conhost' } }, 'first').Id
