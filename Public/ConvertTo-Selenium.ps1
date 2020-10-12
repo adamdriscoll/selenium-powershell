15bedb8fe6d98ab06f5ce1d5e750f531e4673307
ili101 <zoby101@gmail.com>
2020-10-12 22:39:13 +0300

    ConvertTo-Selenium (#175)

---
+++ Public/ConvertTo-Selenium.ps1
@@ -0,0 +1,111 @@
+function ConvertTo-Selenium {
+    <#
+        .SYNOPSIS
+        Convert Selenium IDE .side recording file to PowerShell commands.
+    #>
+    [CmdletBinding()]
+    param (
+        # Path to .side file.
+        [Parameter(Mandatory)]
+        [String]$Path
+    )
+
+    $ByMap = @{
+        id       = 'Id'
+        css      = 'CssSelector'
+        xpath    = 'XPath'
+        linkText = 'LinkText'
+        label    = 'Text'
+        index    = 'Index'
+    }
+    function Get-Replace {
+        <#
+        .SYNOPSIS
+        Helper function to convert ScriptBlocks to strings.
+
+        Parameter set 1:
+        Replace -From [String] with -To [String].
+        -QuotesTo adds quotes around -To.
+        -SplitTo take only the value part from "label=value" of -To.
+
+        Parameter set 2:
+        -By "label=value" replaces input "-By $By" with "-By <Selector> -value '<value>'".
+        * $ByMap is used to map the labels.
+    #>
+        [CmdletBinding()]
+        param (
+            [String]$From,
+            [String]$To,
+            [String]$By,
+            [Parameter(ValueFromPipeline)]
+            $InputObject,
+            [switch]$QuotesTo,
+            [switch]$SplitTo
+        )
+        process {
+            $String = $InputObject.ToString().Trim()
+            if ($From) {
+                if ($QuotesTo) {
+                    $To = '"' + $To + '"'
+                }
+                if ($SplitTo) {
+                    $To = $To.Split('=', 2)[1]
+                }
+                $String = $String.Replace($From, $To)
+            }
+            if ($By) {
+                $String = $String.Replace('-By $By', ('-By {0} -value {1}' -f $ByMap[$By.Split('=', 2)[0]], ('"' + $By.Split('=', 2)[1] + '"')))
+            }
+            $String
+        }
+    }
+
+    $ActionMap = @{
+        click       = { Invoke-SeClick }
+        doubleClick = { Invoke-SeClick -Action DoubleClick }
+        sendKeys    = { Invoke-SeKeys -Keys $Keys }
+        type        = { Invoke-SeKeys -Keys $Keys }
+        select      = { Set-SeSelectValue -By $By }
+    }
+
+    $Recording = Get-Content -Path $Path | ConvertFrom-Json
+    $BaseUrl = [Uri]$Recording.url
+    $PsCode = $(
+        '# Project: ' + $Recording.name
+        foreach ($Test in $Recording.tests) {
+            '# Test: ' + $Test.name
+            foreach ($Command in $Test.commands) {
+                switch ($Command) {
+                    { $_.comment } { '# Description: ' + $_.comment }
+                    { $_.command -eq 'open' } {
+                        $Url = if ([Uri]::IsWellFormedUriString($_.target, [System.UriKind]::Relative)) {
+                            [Uri]::new($BaseUrl, $_.target)
+                        }
+                        else {
+                            $_.target
+                        }
+                        { Set-SeUrl -Url $Url } | Get-Replace -From '$Url' -To $Url -QuotesTo
+                        Break
+                    }
+                    { $_.command -eq 'close' } { { Stop-SeDriver } ; Break }
+                    { $_.command -in $ActionMap.Keys } {
+                        $Action = $ActionMap[$_.command] | Get-Replace -From '$Keys' -To $_.value -QuotesTo -By $_.value
+                        { Get-SeElement -By $By | _Action_ } | Get-Replace -From '_Action_' -To $Action -By $_.target
+                        Break
+                    }
+                    { $_.command -eq 'selectFrame' } {
+                        if ($_.target -eq 'relative=parent') {
+                            { Switch-SeFrame -Parent }
+                        }
+                        else {
+                            { Switch-SeFrame -Frame $Index } | Get-Replace -From '$Index' -To $_.target -SplitTo
+                        }
+                        Break
+                    }
+                    Default { '# Unsupported command. Command: "{0}", Target: "{1}", Value: "{2}", Comment: "{3}".' -f $_.command, $_.target, $_.value, $_.comment }
+                }
+            }
+        }
+    ) | Get-Replace
+    [ScriptBlock]::Create($PsCode -join [Environment]::NewLine)
+}
--- Public/Switch-SeFrame.ps1
+++ Public/Switch-SeFrame.ps1
@@ -13,7 +13,7 @@ function Switch-SeFrame {
     $Driver = Init-SeDriver  -ErrorAction Stop
     #TODO Frame validation... Do not try to switch if element does not exist ?
     #TODO Review ... Maybe Parent / Root should be a unique parameter : -Level Parent/Root )
-    if ($frame) { [void]$Driver.SwitchTo().Frame($Frame) }
+    if ($PSBoundParameters.ContainsKey('Frame')) { [void]$Driver.SwitchTo().Frame($Frame) }
     elseif ($Parent) { [void]$Driver.SwitchTo().ParentFrame() }
     elseif ($Root) { [void]$Driver.SwitchTo().defaultContent() }
 }
