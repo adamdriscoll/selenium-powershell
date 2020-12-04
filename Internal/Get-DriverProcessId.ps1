950e82d19564d25dcef804232ec15f77f93481ee
Francis Mercier <Francis@nzt.ca>
2020-12-04 02:13:46 -0500

    .Where is not a method for CIM Instance object (#190)

--- Internal/Get-DriverProcessId.ps1
+++ Internal/Get-DriverProcessId.ps1
@@ -8,7 +8,7 @@ function Get-DriverProcessId {
 
     if ($IsWindowsPowershell) {
         $Processes = Get-CimInstance -Class Win32_Process -Filter "ParentProcessId=$ServiceProcessId"
-        $BrowserProcess = $Processes.Where( { $_.Name -ne 'conhost.exe' }, 'first').ProcessId
+        $BrowserProcess = $Processes | Where-Object  { $_.Name -ne 'conhost.exe' } | Select-Object -First 1 -ExpandProperty ProcessId
     }
     else {
         $BrowserProcess = (Get-Process).Where( { { $_.Parent.id -eq $ServiceProcessId -and $_.Name -ne 'conhost' } }, 'first').Id
